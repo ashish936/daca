20c1f9cfdee6d90d03734514f8e4421aae46086d
ashish936 <bishtashish936@gmail.com>
2020-10-10 22:35:58 +0530

    added code for the TOS timer

--- script.js
+++ script.js
@@ -1,19 +1,53 @@
 
-var counter = 0;
-var timeleft = 75;
-var time = document.getElementById('timer');
-console.log(time);
+// var counter = 0;
+// var timeleft = 75;
+// var time = document.getElementById('timer');
+// console.log(time);
 
-function convertSeconds(s) {
-    var min = Math.floor(s / 60);
-    var sec = s % 60;
-    return min.toPrecision(2) + ':' + sec.toPrecision(2)
+// function convertSeconds(s) {
+//     var min = Math.floor(s / 60);
+//     var sec = s % 60;
+//     return min.toPrecision(2) + ':' + sec.toPrecision(2)
+// }
+
+// function timeLeft(){
+//     rem = timeleft - counter;
+//     time.innerText = convertSeconds(rem);
+//     counter++;
+// }
+
+// setInterval(timeLeft,1000);
+let counter = 3590;
+let min,hour;
+
+
+function seconds(counter) {
+    // if(counter < 60) return counter;
+    // else {
+    //     //counter = Math.floor(counter/60); // this is for minutes
+    //}
+    return (counter % 60) + 1;
+    
+}
+function minutes(counter) {
+    // let min = 0;
+    // if(counter < 60) return counter =0;
+    // else return (Math.floor(counter/60)) % 60;
+    return ((Math.floor(counter/60)) % 60);
 }
 
-function timeLeft(){
-    rem = timeleft - counter;
-    time.innerText = convertSeconds(rem);
+function hours(counter) {
+    return ((Math.floor(counter/3600)) % 60);
+
+}
+
+function timeLeft() {
+    
     counter++;
+    console.log(counter);
+    const mainTimer = document.querySelector('.main-timer');
+    //mainTimer.innerHTML = counter;
+    mainTimer.innerHTML = `${hours(counter)}h  ${minutes(counter)}m  ${seconds(counter)}s`
 }
 
-setInterval(timeLeft,1000);
+setInterval(timeLeft, 1000);
